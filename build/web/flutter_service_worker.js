694cfe58903a5f74c9380493326232e06c4a9bbe
HANNN-YOLO <reyhanrafaidhil1126@gmail.com>
2025-08-15 09:45:39 +0700

    Fix syntax error in netlify.toml

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c",
-"assets/AssetManifest.bin.json": "99a97df8255cc1642883bc1b4df3e618",
-"assets/AssetManifest.json": "e21f0de3e9ae975381dc3c047df17ba2",
+const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
+"assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "c0ad29d56cfe3890223c02da3c6e0448",
 "assets/NOTICES": "c0a02bbc8f20121e3fc4faffc7d0f9e7",
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6cf347f40e437e7223d6ddea0c32d072",
+"flutter_bootstrap.js": "30c1af04a83d3498ac0a54dc43abf09f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c1abf774f313dd395eb3c070ce7f4aa0",
 "/": "c1abf774f313dd395eb3c070ce7f4aa0",
-"main.dart.js": "38ccb5f5af547b07e5f24ce33be5a1f2",
+"main.dart.js": "d48092ddb8c49734191916698f02ddef",
 "manifest.json": "cafaa640d0d2737820fde7f7a43ebdac",
 "version.json": "00e35b3142e38371ccfb7c0d168e938b"};
 // The application shell files that are downloaded before a service worker can
