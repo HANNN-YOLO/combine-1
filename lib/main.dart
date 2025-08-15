4821d5e117913a628db5f5a81a486f14c376b57b
HANNN-YOLO <reyhanrafaidhil1126@gmail.com>
2025-08-15 09:33:04 +0700

    test ketiga

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'pages/main/landingpage.dart';
 // import 'package:supabase/supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+  // await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    // url: dotenv.env['SUPABASE_URL']!,
+    // anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
   );
   runApp(App());
 }
