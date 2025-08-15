023db47ce1bc5c216a97510c94cba06d1b4f32cc
HANNN-YOLO <reyhanrafaidhil1126@gmail.com>
2025-08-15 20:14:46 +0700

    perubahan 1

--- lib/pages/main/landingpage.dart
+++ lib/pages/main/landingpage.dart
@@ -8,6 +8,17 @@ class Landingpage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.cyan,
         title: Text("Halaman Utama", style: TextStyle(color: Colors.white)),
+        leading: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            image: DecorationImage(
+              image: NetworkImage(
+                "https://i.pinimg.com/1200x/60/00/8a/60008a92a920172636c91365906681d0.jpg",
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
     );
   }
